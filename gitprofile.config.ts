eb3ae31bb3f2c16ecd766036a9e60e06814cb93d
Ryu Gihyeon <95550744+GH1014@users.noreply.github.com>
2024-03-17 20:20:33 +0900

    seo_mod

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -57,21 +57,6 @@ const CONFIG = {
     imageURL: '',
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    researchGate: '',
-    facebook: '',
-    instagram: '',
-    youtube: '', // example: 'pewdiepie'
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    skype: '',
-    telegram: '',
-    website: 'https://www.arifszn.com',
     phone: '',
     email: 'arifulalamszn@gmail.com',
   },
