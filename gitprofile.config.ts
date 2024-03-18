aa4475c088a16e95865132a923387048c7f86b2a
Ryu Gihyeon <95550744+GH1014@users.noreply.github.com>
2024-03-19 02:55:53 +0900

    linkmod

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -38,7 +38,7 @@ const CONFIG = {
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+          link: 'https://drive.google.com/file/d/1ojFIXHWHfNID4Vu90l6iXOVZxM6sOPDw/view?usp=sharing',
         },
         {
           title: 'Project Name',
